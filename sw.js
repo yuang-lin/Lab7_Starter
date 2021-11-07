d1356d542ce5f9188b6b4b4c6668eb98affa7c20
yuang-lin <64988349+yuang-lin@users.noreply.github.com>
2021-11-07 14:42:48 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,9 +2,6 @@
 //         so do not move it next to the other scripts
 
 const CACHE_NAME = 'lab-7-starter';
-var urlsToCache = [
-  '/'
-];
 
 // Once the service worker has been installed, feed it some initial URLs to cache
 self.addEventListener('install', function (event) {
