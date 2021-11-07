d121ea6181efd7a89d1e012cbbdc88e719c11a08
yuang-lin <64988349+yuang-lin@users.noreply.github.com>
2021-11-07 00:09:20 -0700

    Update Router.js

--- assets/scripts/Router.js
+++ assets/scripts/Router.js
@@ -66,5 +66,20 @@ export class Router {
      *     and URL + hash to history
      *  4. Finally, call the stored function for the given page
      */
+    if (!this[page]) {
+      console.error('ERROR: Not Exist Function');
+      return;
+    }
+    var hash;
+    if (page == 'home') {
+      hash = '';
+    }
+    else {
+      hash = '#' + page;
+    }
+    if (!statePopped && window.location.hash != hash) {
+      history.pushState(page, '', window.location + hash);
+    }
+    this[page]();
   }
 }
