360fd355fde3ae85c15d72b2e332b4a21d5477d7
yuang-lin <64988349+yuang-lin@users.noreply.github.com>
2021-11-07 00:53:11 -0700

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -53,10 +53,17 @@ async function init() {
  * of installing it and getting it running
  */
 function initializeServiceWorker() {
-  /**
-   *  TODO - Part 2 Step 1
-   *  Initialize the service worker set up in sw.js
-   */
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
 }
 
 /**
